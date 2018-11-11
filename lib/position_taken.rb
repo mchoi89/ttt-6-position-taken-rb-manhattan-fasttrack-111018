d0801c7baead620a5a3b0e3afc40c5ada330c8f8
mchoi89 <mchoi89@gmail.com>
2018-11-11 01:00:53 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,14 @@
 # code your #position_taken? method here!
 position_taken(board, index)).to be (false)
 
-array[index_number]
-  if index_number == " " || "" || nil
-    return false
-end
+def position_taken?(board, index)
+   if board[index] == nil
+      false
+   elsif board[index] == "" 
+      false
+    elsif board[index] == " "
+      false
+    else board[index] == "X" || "O"
+      true  
+   end
+ end
