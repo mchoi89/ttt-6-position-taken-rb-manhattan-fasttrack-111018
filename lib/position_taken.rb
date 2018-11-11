e19ce55700b186799d754a7c1147d32bddb36b04
mchoi89 <mchoi89@gmail.com>
2018-11-11 00:55:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,2 +1,9 @@
 # code your #position_taken? method here!
 position_taken(board, index)).to be (false)
+
+array[index_number]
+  if index_number == " " || "" || nil
+    return false
+    
+    
+    
